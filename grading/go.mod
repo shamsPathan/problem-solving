513fab226c3a603ce79ab7a5fa37db5a59fcd68e
shams pathan <shamspathan.010@gmail.com>
2024-02-06 21:35:09 +0600

    grading submission

---
+++ grading/go.mod
@@ -0,0 +1,3 @@
+module github.com/bitkohen/ps/grading
+
+go 1.21.0
---
+++ grading/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+func gradingStudents(grades []int32) []int32 {
+	result := make([]int32, 0)
+	pass := int32(38)
+
+	for _, grade := range grades {
+		if grade < pass {
+			result = append(result, grade)
+			continue
+		}
+		nextMultiple := (grade/5 + 1) * 5
+
+		if nextMultiple-grade < 3 {
+			result = append(result, nextMultiple)
+		} else {
+			result = append(result, grade)
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var grades []int32
+
+	for i := 0; i < int(gradesCount); i++ {
+		gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		gradesItem := int32(gradesItemTemp)
+		grades = append(grades, gradesItem)
+	}
+
+	result := gradingStudents(grades)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
